d8b25d496cbe1a801ff6b806ec48c89819f7f9d4
RomanKuzhel <royal.manroman@gmail.com>
2023-03-04 22:33:52 +0000

    lesson.6.py

---
+++ lesson.6.py
@@ -0,0 +1,28 @@
+# my_list=[50, 100, 120, 150, 200]
+# for f in my_list:
+#     if f > 100:
+#         print(f)
+
+# my_list = [33, 88, 123, 200]
+# my_result = []
+# for f in my_list:
+#     my_result = f
+#     if f > 100:
+#        print(my_result)
+
+# my_list = [33, 56, 66, 97]
+# num1 = my_list[-1]
+# num2 = my_list[-2]
+# if len(my_list) < 0:
+#     my_list.append(0)
+# if len(my_list) > 2:
+#     my_list.append(num2+num1)
+#
+# print(my_list)
+
+#Эту строку вы просили переделать с прошлого ДЗ, где я сделал больше пробелов вместо +1
+# string = 'Это пример строки'
+# words = (string.count(' ')+1)
+# print(words)
+
+
